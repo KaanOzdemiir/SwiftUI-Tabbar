35cd7f388925d0f5ebbb8e2a58d8393bc9dbc00d
KaanOzdemir <kaan.ozdemir@tsoft.com.tr>
2021-09-26 17:58:57 +0300

    Initial Commit

---
+++ SwiftUI-Tabbar/SwiftUI_TabbarApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TabbarApp.swift
+//  SwiftUI-Tabbar
+//
+//  Created by Kaan Ozdemir on 26.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TabbarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
